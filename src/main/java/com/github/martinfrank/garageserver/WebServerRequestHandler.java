4ec83c89981303817c81f4053f5afeec06fdc3ac
martinFrank <martin.frank.privat@googlemail.com>
2020-04-01 07:42:29 +0200

    prepare for refactoring

--- src/main/java/com/github/martinfrank/garageserver/WebServerRequestHandler.java
+++ src/main/java/com/github/martinfrank/garageserver/WebServerRequestHandler.java
@@ -35,8 +35,21 @@ public class WebServerRequestHandler implements HttpHandler {
 
     private void createResponse(HttpExchange httpExchange) throws IOException {
         String response = generateWebsite();
-        httpExchange.sendResponseHeaders(200, response.length());
+
+
+//        "    xhttp.setRequestHeader(\"Access-Control-Allow-Headers\", \"http://192.168.0.38:8080\");\n" +
+//                "    xhttp.setRequestHeader(\"Access-Control-Allow-Credentials\", \"true\");\n" +
+
+
         httpExchange.getResponseHeaders().add("Content-Type", "text/html");
+
+//        httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "*");
+//        httpExchange.getResponseHeaders().add("Access-Control-Allow-Header", "*");
+//        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+//        httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods","POST, GET, OPTIONS, DELETE");
+
+        httpExchange.sendResponseHeaders(200, response.length());
+
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes(StandardCharsets.UTF_8));
         os.close();
@@ -60,16 +73,55 @@ public class WebServerRequestHandler implements HttpHandler {
     }
 
     private String generateWebsite() {
+
         //https://strobelstefan.org/?p=5772
 
-        final StringBuilder content = new StringBuilder("<!doctype html><html><head/><body>");
-        content.append("<div align=\"center\"><embed src=\"http://192.168.0.64:8081\" style=\"width:680px; height: 520px;\"></div><br/>");
-        content.append("<div align=\"center\"><form method=\"post\">");
-        for (HtmlSubmitButton button : buttons) {
-            content.append(button.createSubmitButtonHtmlString());
-        }
-        content.append("</form></div>");
-        content.append("</body></html>");
-        return content.toString();
+//        final StringBuilder content = new StringBuilder("<!doctype html><html><head/><body>");
+//        content.append("<div align=\"center\"><embed src=\"http://192.168.0.64:8081\" style=\"width:680px; height: 520px;\"></div><br/>");
+//        content.append("<div align=\"center\"><form method=\"post\">");
+//        for (HtmlSubmitButton button : buttons) {
+//            content.append(button.createSubmitButtonHtmlString());
+//        }
+//        content.append("</form></div>");
+//        content.append("</body></html>");
+//        return content.toString();
+
+        String str = "{ \"id\" : 2,  \"content\" : \"Hello there, Stranger!\" + }";
+
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<script type=\"text/javascript\" language=\"javascript\">\n" +
+                "function UserAction() {\n" +
+                "    var xhttp = new XMLHttpRequest();\n" +
+//                "    xhttp.onreadystatechange = function() {\n" +
+//                "         if (this.readyState == 4 && this.status == 200) {\n" +
+//                "             alert(this.responseText);\n" +
+//                "         }\n" +
+//                "    };\n" +
+//                "    xhttp.open(\"POST\", \"http://192.168.0.38:8080/hello-world\", true);\n" + // false for synchronous request
+                "    xhttp.open(\"GET\", \"http://192.168.0.38:8080/hello-world\", false);\n" +  // false for synchronous request
+//                "    xhttp.setRequestHeader(\"Access-Control-Allow-Headers\", \"http://192.168.0.38:8080\");\n" +
+//                "    xhttp.setRequestHeader(\"Access-Control-Allow-Credentials\", \"true\");\n" +
+                "    xhttp.setRequestHeader(\"Content-type\", \"application/json\");\n" +
+//                "    xhttp.send(\"{ \\\"id\\\" : 2,  \\\"content\\\" : \\\"Hello there, Stranger!\\\" }\");\n" +
+                "    xhttp.send( null );\n" +
+                "    return xhttp.responseText;\n" +
+                "}" +
+                "</script>" +
+                "<body>\n" +
+                "\n" +
+                "<h2>My First JavaScript</h2>\n" +
+                "\n" +
+//                "<button type=\"submit\" onclick=\"UserAction()\">Search</button>\n"+
+                "<button type=\"submit\" onclick=\"document.getElementById('demo2').innerHTML = UserAction()\">Search</button>\n" +
+                "<button type=\"button\"\n" +
+                "onclick=\"document.getElementById('demo').innerHTML = Date()\">\n" +
+                "Click me to display Date and Time.</button>\n" +
+                "\n" +
+                "<p id=\"demo\"></p>\n" +
+                "<p id=\"demo2\"></p>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html> ";
     }
 }
