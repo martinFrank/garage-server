acf41a6ac2e59952166c19d9ab563dd700e68041
Martin Frank <martin.frank.privat@googlemail.com>
2022-05-04 08:02:44 +0200

    removed trash; trimmed to minimum; update to current service situation

--- src/main/resources/template.js
+++ src/main/resources/template.js
@@ -6,7 +6,7 @@
         function restCall(method, path, target) {
             var request = new XMLHttpRequest();
             const cnt = '{"id":1,"content":"Hello, Stranger!"}';
-            request.open(method,"http://192.168.0.66:8080"+path);
+            request.open(method,"http://192.168.0.64:8082"+path);
             request.setRequestHeader("CONTENT-TYPE","application/json");
             request.setRequestHeader("Cache-Control","max-age=0, no-cache, no-store, must-revalidate");
             request.addEventListener('load', function(event) {
@@ -27,28 +27,13 @@
 
 <div align="center"><h2>Garage Server</h2></div>
 <br/>
-
+<div align="center"> <iframe src="http://192.168.0.64:8088" title="Garage Web Cam" width="640" height="480"></iframe> </div>
+<br/>
 <div align="center">
-    <button type="button" onclick="restCall('GET', '/garage/gate', 'gateStatus')" > <!--style="height:100px;width:300px" > -->
-        garage status
-    </button>
     <button type="button" onclick="restCall('POST', '/garage/gate', 'gateStatus')" > <!--style="height:100px;width:300px"> -->
         garage change
     </button>
-    <br/>
-    <button type="button" onclick="restCall('GET', '/garage/light', 'lightStatus')" > <!--style="height:100px;width:300px" > -->
-        light status
-    </button>
-    <button type="button" onclick="restCall('GET', '/garage/light', 'lightStatus')" > <!--style="height:100px;width:300px"> -->
-        light change
-    </button>
 </div>
-
-<br/>
-<div align="center">
-    <p id="gateStatus">{"state":"unknown","lastRequest":"unknown","isMoving":false}<br/>
-    <p id="lightStatus">{"state":"unknown","lastRequest":"unknown","isMoving":false}<br/>
-</p></div>
 
 </body>
 </html>
